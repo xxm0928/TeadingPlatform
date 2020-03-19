88fc14dad0c7bc6f47edf05e5ab36114e8058edb
yangxing936 <62365025+yangxing936@users.noreply.github.com>
2020-03-19 18:23:41 +0800

    杨兴添加了一个用户api接口

---
+++ TeadingPlatformAPI/TeadingPlatformAPI/Controllers/UserApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TeadingPlatformAPI.Controllers
+{
+    public class UserApiController : ApiController
+    {
+        //测试
+        public int Login()
+        {
+            return 1;
+        }
+    }
+}
