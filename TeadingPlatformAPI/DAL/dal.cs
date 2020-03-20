7e1865ff62c3ab3205c2402c4cd214a20fc7f1f6
yangxing936 <62365025+yangxing936@users.noreply.github.com>
2020-03-20 19:53:19 +0800

    1

--- TeadingPlatformAPI/DAL/dal.cs
+++ TeadingPlatformAPI/DAL/dal.cs
@@ -10,7 +10,7 @@ namespace DAL
 {
     public class Dal
     {
-        
+        DBHelper dBHelper = new DBHelper();
         /// <summary>
         /// 用户登录
         /// data 是统一参数值
@@ -18,11 +18,31 @@ namespace DAL
         /// </summary>
         /// <param name="data"></param>
         /// <returns></returns>
-        public UnitedReturn UserLogin(object data)
+        public UnitedReturn UserLogin(object data)//这是模板 登录重新写账号密码 此类只作为参考
         {
-
-            var res = string.Format("$select * from UserInfo u where u.Uname='{}' and u.Upwd='{}'");
-            return dal.UserLogin(data);
+            //把data转换成类的类型 
+            UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(data.ToString());
+            //sql语句
+            var sql = string.Format($"$select count(1) from UserInfo u where u.Uname='{userInfo.UserName}'");
+            //返回值
+            var res =Convert.ToInt32( dBHelper.ExecuteScalar(sql));
+            UnitedReturn united = new UnitedReturn();
+            //如果登陆成功 给统一返回类型的model赋值
+            if (res > 0)
+            {
+                //给统一返回类型的model赋值
+                united.data = null;//返回的数据
+                united.msg = "登陆成功";//返回的字符串
+                united.res = 1;//返回的int值
+            }
+            else
+            {
+                //给统一返回类型的model赋值
+                united.data = null;
+                united.msg = "登陆失败";
+                united.res = 0;
+            }
+            return united;
         }
     }
 }
--- TeadingPlatformAPI/TeadingPlatformAPI/Controllers/UserApiController.cs
+++ TeadingPlatformAPI/TeadingPlatformAPI/Controllers/UserApiController.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Model;
 using BLL;
 namespace TeadingPlatformAPI.Controllers
 {
