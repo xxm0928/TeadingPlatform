1f1837877c07cc9a3d3f69176560d3d776f2520b
yyy965497752 <yanchong6535@163.com>
2020-03-20 21:55:37 +0800

    ycx ....

---
+++ TeadingPlatformMVC/TeadingPlatformMVC/Controllers/YcxCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeadingPlatformMVC.Controllers
+{
+    public class YcxCodeController : Controller
+    {
+        // GET: YcxCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
