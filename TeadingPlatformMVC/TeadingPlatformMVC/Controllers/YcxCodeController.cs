615686a89e7e2702b33cf5c7095729a2f11b10ab
yyy965497752 <yanchong6535@163.com>
2020-03-20 22:02:15 +0800

    ycx du

--- TeadingPlatformMVC/TeadingPlatformMVC/Controllers/YcxCodeController.cs
+++ TeadingPlatformMVC/TeadingPlatformMVC/Controllers/YcxCodeController.cs
@@ -13,5 +13,9 @@ namespace TeadingPlatformMVC.Controllers
         {
             return View();
         }
+        public ActionResult UserSelect()
+        {
+            return View();
+        }
     }
 }
