bf9d77d1cfadb480b440cfacd08d7bdc6e6d3602
yangxing936 <62365025+yangxing936@users.noreply.github.com>
2020-03-20 19:23:47 +0800

    杨兴更改了框架

--- TeadingPlatformAPI/DAL/dal.cs
+++ TeadingPlatformAPI/DAL/dal.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
 namespace DAL
 {
-    public class dal
+    public class Dal
     {
+        
+        /// <summary>
+        /// 用户登录
+        /// data 是统一参数值
+        /// UnitedReturn 是统一返回的类型
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public UnitedReturn UserLogin(object data)
+        {
+
+            var res = string.Format("$select * from UserInfo u where u.Uname='{}' and u.Upwd='{}'");
+            return dal.UserLogin(data);
+        }
     }
 }
