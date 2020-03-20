aa42790be501088320399c0737a4d6c5925043c1
zhaozhao-zh <62336943+zhaozhao-zh@users.noreply.github.com>
2020-03-20 18:46:26 +0800

    model字段及注释

---
+++ TeadingPlatformAPI/Model/Orderform.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+     public class Orderform
+    {
+        public int OrderformId { get; set; }
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public int CommodityId { get; set; }
+        /// <summary>
+        /// 商品编号
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        public string OrderNumser { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        public int CommodityCount { get; set; }
+        /// <summary>
+        /// 购买数量
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// 消费金额
+        /// </summary>
+        public int OrderState { get; set; }
+        /// <summary>
+        /// 订单状态
+        /// </summary>
+        public int LogisticsId { get; set; }
+        /// <summary>
+        ///物流编号
+        /// </summary>
+    }
+}
---
+++ TeadingPlatformAPI/Model/UserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// 用户昵称
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 用户头像
+        /// </summary>
+        public string UserPhoto { get; set; }
+        /// <summary>
+        /// 用户性别
+        /// </summary>
+        public int UserSex { get; set; }
+        /// <summary>
+        /// 店铺Id
+        /// </summary>
+        public int ShopId { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string UserNumder { get; set; }
+        /// <summary>
+        /// 用户年龄
+        /// </summary>
+        public int UserAge { get; set; }
+        /// <summary>
+        /// 身份证
+        /// </summary>
+        public string UserIDNumber { get; set; }
+    }
+}
