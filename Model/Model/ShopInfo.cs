043f4fab2024e95dddbb7727ca3f8fc0363b15c8
zhaozhao-zh <62336943+zhaozhao-zh@users.noreply.github.com>
2020-03-20 15:19:25 +0800

    Revert "Revert "model字段及注释""
    This reverts commit a7df3636d9c74c79b362d6ecb29869d14c780bbd.

---
+++ Model/Model/ShopInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public  class ShopInfo
+    {
+        /// <summary>
+        /// 店铺Id
+        /// </summary>
+        public int ShopId { get; set; }
+        /// <summary>
+        /// 店铺名称
+        /// </summary>
+        public string ShopName { get; set; }
+        /// <summary>
+        /// 商品Id
+        /// </summary>
+        public int CommodityId { get; set; }
+    }
+}
---
+++ Model/Model/TypeInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class TypeInfo
+    {
+        /// <summary>
+        /// 类型编号
+        /// </summary>
+        public int TypeId { get; set; }
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string TypeName { get; set; }
+    }
+}
