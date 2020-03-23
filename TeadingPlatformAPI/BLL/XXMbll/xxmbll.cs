5acf0dd2b6853730d7559fec895e0791d28135f4
许学�?	email = 2041314558@qq.com <Dell@DESKTOP-0U20KQ5>
2020-03-23 15:16:25 +0800

    xxm改后

---
+++ TeadingPlatformAPI/BLL/XXMbll/xxmbll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DAL;
+
+namespace BLL.XXMbll
+{
+    public class xxmbll
+    {
+        XXMdal XXMdal = new XXMdal();
+        /// <summary>
+        /// 添加商品
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public UnitedReturn Commodity(object data)
+        {
+            try
+            {
+                return XXMdal.Commodity(data);
+            }
+            catch (Exception ex)
+            {
+                //错误日志 log4net
+                //1请求接口
+                //2请求参数
+                //3错误信息
+                //4请求时间
+
+                return new UnitedReturn() { data = ex.InnerException.Message, res = -1, msg = ex.Message };
+            }
+        }
+    }
+}
--- TeadingPlatformAPI/BLL/bll.cs
+++ TeadingPlatformAPI/BLL/bll.cs
@@ -11,6 +11,7 @@ namespace BLL
     public class Bll
     {
         Dal dal = new Dal();
+
         /// <summary>
         /// 用户登录
         /// data 是统一参数值
@@ -22,36 +23,7 @@ namespace BLL
         {
             return dal.UserLogin(data);
         }
-       
-            #region 3/20 20:25
-            /// <summary>
-            /// 通过 bll 传递数据
-            /// </summary>
-            /// <returns></returns>
-            //public List<UserInfo> PersonalInformation()
-            //{
-            //    dal d = new dal();
-            //    List<UserInfo> list = d.PersonalInformation();
-            //    return list.ToList();
 
-
-            //}
-            /// <summary>
-            /// 通过 bll 传递数据
-            /// </summary>
-            /// <param name="model"></param>
-            /// <returns></returns>
-            //public int AddPersonalInformation(UserInfo model)
-            //{
-            //    dal d = new dal();
-            //    int result=d.AddPersonalInformation(model);
-            //    return result;
-            //}
-
-            #endregion
-
-
-       
         /// <summary>
         /// 用户注册
         /// </summary>
@@ -61,5 +33,4 @@ namespace BLL
         {
             return dal.UserAdd(data);
         }
-    }
 }
