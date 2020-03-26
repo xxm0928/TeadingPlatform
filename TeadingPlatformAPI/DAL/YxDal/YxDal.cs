adfac6183b7630a1e2030ff75b99e86e28a26768
yangxing936 <62365025+yangxing936@users.noreply.github.com>
2020-03-26 08:29:10 +0800

    123

--- TeadingPlatformAPI/DAL/YxDal/YxDal.cs
+++ TeadingPlatformAPI/DAL/YxDal/YxDal.cs
@@ -105,7 +105,7 @@ namespace DAL
         }
         /// <summary>
         /// 修改订单状态 发货 State=1 待付款  2 待发货 3 已发货 4 已签收 5 已完成
-        /// </summary>
+        /// </summary>11
         /// <param name="data"></param>
         /// <returns></returns>
         public UnitedReturn UpdateOrderState(object data)
