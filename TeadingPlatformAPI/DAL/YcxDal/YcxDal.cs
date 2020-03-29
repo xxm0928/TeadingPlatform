775cf4f7bcc84a3caf99ae176d3ea23946e8cbd2
yyy965497752 <yanchong6535@163.com>
2020-03-29 21:04:51 +0800

    ycx_改_接口

--- TeadingPlatformAPI/DAL/YcxDal/YcxDal.cs
+++ TeadingPlatformAPI/DAL/YcxDal/YcxDal.cs
@@ -1,4 +1,5 @@
 ﻿using Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +10,84 @@ namespace DAL
 {
     public class YcxDal
     {
+        Ycx_Helper help = new Ycx_Helper();
+        DBHelper helpX = new DBHelper();
         #region ycx_3/20 20:10
         /// <summary>
         /// 获取集合数据
         /// </summary>
         /// <returns></returns>
-        public List<UserInfo> PersonalInformation()
+        public UnitedReturn PersonalInformation(object data)
         {
-            Ycx_Helper help = new Ycx_Helper();
-            string textSql = "exec UserInfo_proc_Select";
-            List<UserInfo> list = help.Getlist<UserInfo>(textSql);
-            return list.ToList();
+            try
+            {
+                var textSql = "exec UserInfo_proc_Select";
+                var ListT = helpX.GetToList<TypeInfo>(textSql);
+                UnitedReturn united = new UnitedReturn();
+                if (ListT !=null && ListT.Count>0)
+                {
+                    united.data = ListT;
+                    united.msg = "成功！";
+                    united.res = 1;
+                }
+                else
+                {
+                    united.data = null;
+                    united.msg = "失败！";
+                    united.res = 0; 
+                }
+                return united;
+            }
+            catch (Exception ex)
+            {
+
+                return new UnitedReturn() { data = ex.InnerException.Message, res = -1, msg = ex.Message };
+            }
+            //Ycx_Helper help = new Ycx_Helper();
+            ////string textSql = "exec UserInfo_proc_Select";
+            //List<UserInfo> list = help.Getlist<UserInfo>(textSql);
+            //return list.ToList();
         }
         /// <summary>
         /// 获取行
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
-        public int addpersonalinformation(UserInfo model)
+        public UnitedReturn addpersonalinformation(UserInfo model, object data)
         {
-            
-            Ycx_Helper help = new Ycx_Helper();
-            string textsql = $"exec userinfo_proc_add '{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
-            int result = help.GetLine(textsql);
-            return result;
+
+            try
+            {
+                if (data.ToString()!="System.object" && data.ToString()!="1")
+                {
+                    model = JsonConvert.DeserializeObject<UserInfo>(JsonConvert.SerializeObject(data));
+                }
+                var textsql = $"exec userinfo_proc_add '{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
+                var result = helpX.ExecuteNonQuery(textsql);
+                UnitedReturn united = new UnitedReturn();
+                if (result > 0)
+                {
+                    united.data = null;
+                    united.msg = "添加成功";
+                    united.res = 1;
+                }
+                else
+                {
+                    united.data = null;
+                    united.msg = "添加失败";
+                    united.res = 0;
+                }
+                return united;
+            }
+            catch (Exception ex)
+            {
+
+                return new UnitedReturn() { data = ex.InnerException.Message, res = -1, msg = ex.Message };
+            }
+
+            //string textsql = $"exec userinfo_proc_add '{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
+            //int result = help.GetLine(textsql);
+
         }
         #endregion
         /// <summary>
@@ -41,16 +96,43 @@ namespace DAL
         /// <param name="model"></param>
         /// <param name="id"></param>
         /// <returns></returns>
-        public int ExitPersonalInformation(UserInfo model,int id)
+        public UnitedReturn ExitPersonalInformation(UserInfo model, int id,object data)
         {
-            string sql = $"exec UserInfo_proc_Exit {id},'{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
-            Ycx_Helper help = new Ycx_Helper();
-            
-            int result =help.GetLine(sql);
-            return result;
+            try
+            {
+                var sql = $"exec UserInfo_proc_Exit {id},'{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
+                var res = helpX.ExecuteNonQuery(sql);
+                UnitedReturn united = new UnitedReturn();
+                if (res>0)
+                {
+                    united.data = res;
+                    united.msg = "修改成功";
+                    united.res = 1;
+
+                }
+                else
+                {
+                    united.data = null;
+                    united.msg = "修改失败";
+                    united.res = 0;
+                }
+                return united;
+            }
+            catch (Exception ex)
+            {
+
+                return new UnitedReturn() { data = ex.InnerException.Message, res = -1, msg = ex.Message };
+            }
+            //string sql = $"exec UserInfo_proc_Exit {id},'{model.UserName}','{model.UserPhoto}',{model.UserSex},{model.ShopId},'{model.UserNumder}','{model.UserAge}','{model.UserIDNumber}'";
+            //Ycx_Helper help = new Ycx_Helper();
+
+            //int result = help.GetLine(sql);
+            //return result;
 
 
         }
+       
+
     }
 
 
