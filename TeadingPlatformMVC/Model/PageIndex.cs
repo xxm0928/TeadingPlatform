d94a0f49d23913859b400f43db43a63df8760602
许学�?	email = 2041314558@qq.com <Dell@DESKTOP-0U20KQ5>
2020-04-09 09:08:17 +0800

    120

---
+++ TeadingPlatformMVC/Model/PageIndex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PageIndex
+    {
+        public int Count { get; set; }
+        public int Size { get; set; }
+        public double Page { get; set; }
+    }
+}
